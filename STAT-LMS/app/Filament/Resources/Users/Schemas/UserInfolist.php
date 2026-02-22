aa299f7f5d0c7a9f9f78c8664004c5b35bae2dca
aguitezFRG <fpaguitez@up.edu.ph>
2026-02-23 01:18:45 +0800

    chore: added relevant details to the user infolist

--- STAT-LMS/app/Filament/Resources/Users/Schemas/UserInfolist.php
+++ STAT-LMS/app/Filament/Resources/Users/Schemas/UserInfolist.php
@@ -4,7 +4,6 @@ namespace App\Filament\Resources\Users\Schemas;
 
 use App\Models\User;
 use Filament\Infolists\Components\TextEntry;
-use Filament\Infolists\Components\TextColumn;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
@@ -17,22 +16,26 @@ class UserInfolist
                 Section::make('User Profile')
                     ->components([
                         TextEntry::make('id')
-                            ->label('UUID'),
+                            ->label('UUID')
+                            ->copyable(),
 
                         TextEntry::make('f_name')
                             ->label('First Name'),
 
                         TextEntry::make('m_name')
-                        ->label('Middle Name'),
+                            ->label('Middle Name')
+                            ->placeholder('N/A'),
 
                         TextEntry::make('l_name')
                             ->label('Last Name'),
 
                         TextEntry::make('std_number')
-                            ->label('Student Number'),
+                            ->label('Student Number')
+                            ->placeholder('N/A'),
 
                         TextEntry::make('email')
-                            ->label('Email Address'),
+                            ->label('Email Address')
+                            ->icon('heroicon-m-envelope'),
 
                         TextEntry::make('role')
                             ->badge()
@@ -51,6 +54,29 @@ class UserInfolist
                                 'committee' => 'Committee',
                                 default => ucfirst($state),
                             }),
+                    ])->columns(2),
+
+                    Section::make('Account Details')
+                    ->components([
+                        TextEntry::make('created_at')
+                            ->label('Account Created')
+                            ->dateTime('F d, Y h:i A'),
+
+                        TextEntry::make('updated_at')
+                            ->label('Last Updated')
+                            ->dateTime('F d, Y h:i A'),
+
+                        TextEntry::make('deleted_at')
+                            ->label('Soft Deleted At')
+                            ->dateTime('F d, Y h:i A')
+                            ->placeholder('Not Deleted')
+                            ->color('danger'),
+
+                        TextEntry::make('revoked_at')
+                            ->label('Access Revoked At')
+                            ->dateTime('F d, Y h:i A')
+                            ->placeholder('Active Account')
+                            ->color('warning'),
                     ])->columns(2),
             ]);
     }
