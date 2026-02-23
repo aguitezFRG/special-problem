90080b3083745c94d81291d435c6176d4f92a1d8
aguitezFRG <fpaguitez@up.edu.ph>
2026-02-24 00:55:32 +0800

    feat: used .env for RBAC

---
+++ STAT-LMS/config/api.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'level_1_access_roles' => explode(',', env('LEVEL_1_ACCESS_ROLES')),
+    'level_2_access_roles' => explode(',', env('LEVEL_2_ACCESS_ROLES')),
+    'level_3_access_roles' => explode(',', env('LEVEL_3_ACCESS_ROLES')),
+];
