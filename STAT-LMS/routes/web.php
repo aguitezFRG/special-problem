f90a7f15ea79c746519d66ba1491d220ccb46fdd
aguitezFRG <fpaguitez@up.edu.ph>
2026-02-27 13:02:21 +0800

    feat: added route for viewing documents

--- STAT-LMS/routes/web.php
+++ STAT-LMS/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MaterialStreamController;
 
 Route::get('/', function () {
     return redirect('/admin');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/materials/stream/{record}', [MaterialStreamController::class, 'stream'])
+        ->name('materials.stream');
 });
