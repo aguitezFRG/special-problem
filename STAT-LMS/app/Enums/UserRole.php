a1852d29f43b4e22f8b831cec047698ac8402607
aguitezFRG <fpaguitez@up.edu.ph>
2026-02-27 16:01:16 +0800

    feat: implemented a UserRole enum

---
+++ STAT-LMS/app/Enums/UserRole.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Enums;
+
+use Filament\Support\Contracts\HasLabel;
+use Filament\Support\Contracts\HasColor;
+
+enum UserRole: string implements HasLabel, HasColor
+{
+    case COMMITTEE = 'committee';
+    case IT = 'it';
+    case RR = 'staff/custodian';
+    case FACULTY = 'faculty';
+    case STUDENT = 'student';
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::IT => 'IT Administrator',
+            self::COMMITTEE => 'Reading Room Committee',
+            self::RR => 'Staff/Custodian',
+            self::FACULTY => 'Faculty Member',
+            self::STUDENT => 'Student User',
+        };
+    }
+
+    public function getColor(): string | array | null
+    {
+        return match ($this) {
+            self::IT => 'info',
+            self::COMMITTEE => 'success',
+            self::RR => 'warning',
+            self::FACULTY => 'primary',
+            self::STUDENT => 'gray',
+        };
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function getAccessLevel(): int
+    {
+        return match ($this) {
+            self::COMMITTEE => 3,
+            self::IT => 3,
+            self::RR => 2,
+            self::FACULTY => 2,
+            self::STUDENT => 1,
+        };
+    }
+}
