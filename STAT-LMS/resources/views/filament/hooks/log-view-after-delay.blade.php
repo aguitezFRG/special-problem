a77244c6e1a92495c8c7a78a8910adaeea7ca74a
aguitezFRG <fpaguitez@up.edu.ph>
2026-03-06 17:31:38 +0800

    feat: added a blade file that works in the background for logging material copy views

---
+++ STAT-LMS/resources/views/filament/hooks/log-view-after-delay.blade.php
@@ -0,0 +1,26 @@
+<div x-data="logViewTracker('{{ $wireId }}')"></div>
+
+<script>
+document.addEventListener('alpine:init', () => {
+    Alpine.data('logViewTracker', (wireId) => ({
+        logged: false,
+        timer: null,
+        init() {
+            const wire = Livewire.find(wireId);
+            if (!wire) return;
+
+            this.timer = setTimeout(() => {
+                if (!this.logged) {
+                    this.logged = true;
+                    wire.dispatch('logView');
+                    // console.log('Logged view after delay');
+                }
+            }, 5000);
+
+            document.addEventListener('visibilitychange', () => {
+                if (document.hidden) clearTimeout(this.timer);
+            });
+        }
+    }));
+});
+</script>
