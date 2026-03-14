4ddaeb8418ad8167f9ae959fc9cf306ab0e07cc4
aguitezFRG <fpaguitez@up.edu.ph>
2026-03-14 18:29:24 +0800

    policy: updated requirements for accomplishing some operations

--- STAT-LMS/app/Policies/MaterialAccessEventsPolicy.php
+++ STAT-LMS/app/Policies/MaterialAccessEventsPolicy.php
@@ -39,12 +39,28 @@ class MaterialAccessEventsPolicy
      */
     public function update(User $user, MaterialAccessEvents $materialAccessEvents): bool
     {
-        return in_array($user->role, [UserRole::COMMITTEE->value, UserRole::IT->value]);
+        $user_role = $user->role;
+
+        $updated_by = $materialAccessEvents->approver_id;
+
+        if ($updated_by == null) {
+            // If the event has never been updated, only allow committee and IT to update
+            return in_array($user_role, [UserRole::COMMITTEE->value, UserRole::IT->value]);
+        }
+
+        // Allow the user who last updated the event to update it again, as well as committee and IT
+        return $user->id === $updated_by || in_array($user_role, [UserRole::COMMITTEE->value, UserRole::IT->value]);
     }
 
+    /**
+     * Determine whether the user can delete any models.
+     */
     public function deleteAny(User $user): bool
     {
-        return in_array($user->role, [UserRole::COMMITTEE->value, UserRole::IT->value]);
+        // return in_array($user->role, [UserRole::COMMITTEE->value, UserRole::IT->value]);
+
+        // TO CLARIFY: No one should be able to delete material access events
+        return false;
     }
 
     /**
@@ -52,9 +68,15 @@ class MaterialAccessEventsPolicy
      */
     public function delete(User $user, MaterialAccessEvents $materialAccessEvents): bool
     {
-        return in_array($user->role, [UserRole::COMMITTEE->value, UserRole::IT->value]);
+        // return in_array($user->role, [UserRole::COMMITTEE->value, UserRole::IT->value]);
+
+        // TO CLARIFY: No one should be able to delete material access events
+        return false;
     }
 
+    /**
+     * Determine whether the user can restore any models.
+     */
     public function restoreAny(User $user): bool
     {
         return in_array($user->role, [UserRole::COMMITTEE->value, UserRole::IT->value]);
@@ -66,6 +88,11 @@ class MaterialAccessEventsPolicy
     public function restore(User $user, MaterialAccessEvents $materialAccessEvents): bool
     {
         return in_array($user->role, [UserRole::COMMITTEE->value, UserRole::IT->value]);
+    }
+
+    public function forceDeleteAny(User $user): bool
+    {
+        return false; // No one can permanently delete material access events
     }
 
     /**
