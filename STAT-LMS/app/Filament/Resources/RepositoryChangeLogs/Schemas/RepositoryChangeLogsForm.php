a2e371a8b3fee94ef8798e312617e63c7aa99490
aguitezFRG <fpaguitez@up.edu.ph>
2026-03-15 00:31:32 +0800

    chore: overhauled the content of the repositorychangelog form, but also disabled all input fields

--- STAT-LMS/app/Filament/Resources/RepositoryChangeLogs/Schemas/RepositoryChangeLogsForm.php
+++ STAT-LMS/app/Filament/Resources/RepositoryChangeLogs/Schemas/RepositoryChangeLogsForm.php
@@ -2,10 +2,13 @@
 
 namespace App\Filament\Resources\RepositoryChangeLogs\Schemas;
 
-use Filament\Forms\Components\DateTimePicker;
+use App\Enums\RepositoryChangeType;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\KeyValue;
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Section;
 
 class RepositoryChangeLogsForm
 {
@@ -13,23 +16,48 @@ class RepositoryChangeLogsForm
     {
         return $schema
             ->components([
-                TextInput::make('editor_id')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('rr_material_id')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('target_user_id')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('table_changed')
-                    ->required(),
-                TextInput::make('change_type')
-                    ->required(),
-                Textarea::make('change_made')
-                    ->columnSpanFull(),
-                DateTimePicker::make('changed_at')
-                    ->required(),
+                Section::make('Change Overview')
+                    ->schema([
+                        TextInput::make('editor_id')
+                            ->label('Editor')
+                            ->disabled(),
+
+                        TextInput::make('table_changed')
+                            ->label('Table Changed')
+                            ->disabled(),
+
+                        Select::make('change_type')
+                            ->label('Change Type')
+                            ->options(collect(RepositoryChangeType::cases())
+                                ->mapWithKeys(fn ($case) => [$case->value => $case->getLabel()]))
+                            ->disabled(),
+                    ])
+                    ->columns(3),
+
+                Section::make('Related Records')
+                    ->schema([
+                        TextInput::make('rr_material_id')
+                            ->label('Related Material')
+                            ->disabled(),
+
+                        TextInput::make('target_user_id')
+                            ->label('Target User')
+                            ->disabled(),
+
+                        DateTimePicker::make('changed_at')
+                            ->label('Changed At')
+                            ->disabled(),
+                    ])
+                    ->columns(3),
+
+                Section::make('Change Details')
+                    ->schema([
+                        KeyValue::make('change_made')
+                            ->label('Changes Made')
+                            ->keyLabel('Field')
+                            ->valueLabel('Value')
+                            ->disabled(),
+                    ]),
             ]);
     }
 }
